1cc2b10666339b7a5bdb34722e8ced6b85d220f4
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-10 17:31:16 +0100

    Add - print results with dynamic request

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,25 @@ export default {
   components:{
     store,
     Header,
-    Main
+    Main,
   },
   data(){
     return {
       store,
+      API_KEY:'fec66e332824c749357e56b0b5da4346',
     }
   },
   methods: {
     searchFilm() {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=fec66e332824c749357e56b0b5da4346&query=ritorno+al+futuro')
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}&query=${this.store.query}`)
       .then(res => {
         this.store.films = res.data.results
         console.log(this.store.films)
       })
     }
+  },
+  created() {
+    console.log(this.store.query)
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,21 @@
 <script>
+import { store } from '/store'
+
+export default {
+    components:{
+    store,
+  },
+  data(){
+    return {
+      store,
+    }
+  },
+}
 </script>
 
 <template>
 <button @click="$emit('search')">Cerca</button>
-<input type="text">
+<input type="text" v-model="store.query" >
 </template>
 
 <style>
