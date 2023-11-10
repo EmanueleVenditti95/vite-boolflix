7347c8854d8275314c340d050da48d4207460181
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-10 15:26:24 +0100

    Add - store.js,header layout with fake button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
-<script setup>
+<script>
+import { store } from '../store';
+import Header from './components/Header.vue';
+import axios from 'axios';
 
+export default {
+  components:{
+    store,
+    Header
+  },
+  data(){
+    return {
+      store,
+    }
+  },
+  methods: {
+    searchFilm() {
+      console.log('cerca film')
+    }
+  }
+}
 </script>
 
 <template>
-
+<Header @search="searchFilm" />
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+<button @click="$emit('search')">Cerca</button>
+<input type="text">
+</template>
+
+<style>
+</style>
